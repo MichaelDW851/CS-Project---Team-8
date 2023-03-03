bf72f2343898826fd566cd029913b3e568178576
adria <adriansanchez68.as@gmail.com>
2023-03-03 13:19:03 -0600

    New Main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Create a JFrame object, which represents a window
+        JFrame frame = new JFrame("My Swing App");
+
+        // Set the size of the window
+        frame.setSize(400, 300);
+
+        // Set the default close operation (exit when the user clicks the close button)
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // Create a JLabel object, which represents a text label
+        JLabel label = new JLabel("Hello, world!");
+
+        // Add the label to the content pane of the window
+        frame.getContentPane().add(label);
+
+        // Display the window
+        frame.setVisible(true);
     }
 }
